95e2140f59567a9dfe9c342534afcc1723f2291c
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-12 15:30:22 +0200

    x

--- app.r
+++ app.r
@@ -1,7 +1,7 @@
 #lees shape in en sla op als shape.rds
 source('lees_shape.r')
 #reken de coordinaten om naar wsg en sla op als shape_wgs.rds
-source('paralel.r')
+source('omrekenen_shape.r')
 #teken op de kaart
 source('leaflet.r')
 
