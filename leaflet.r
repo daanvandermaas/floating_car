f8031316cd4bf6bf7fc21fc8e379c8dedade3da3
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-12 16:07:15 +0200

    x

--- leaflet.r
+++ leaflet.r
@@ -2,6 +2,8 @@
 
 shape = readRDS('db/shape_wgs.rds')
 #shape = readRDS('db\shape_wgs.rds')
+
+shape@data$goed = NA
      
 for(i in 1:nrow(shape@data)){
 
@@ -16,6 +18,8 @@ map = leaflet(coor)
 map = addTiles(map)
 
 map =   addPolylines(map)
+map = setView(map, zoom = 15, lat = x[1] , lng=y[1] )
 print(map)
-browser()
+
+shape@data$goed[i] = readline(prompt="goed =1 fout = 2?: ")
 }
