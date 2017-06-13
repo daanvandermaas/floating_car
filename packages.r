f21ae3e38bcfcbf23134baf4322af9a5dddab165
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-13 10:05:36 +0200

    x

--- packages.r
+++ packages.r
@@ -13,17 +13,3 @@ install_github("wch/webshot")
 library(htmlwidgets)
 library(webshot)
 library(mapview)
-## create map
-
-## save html to png
-saveWidget(map, "temp.html", selfcontained = FALSE)
-webshot("temp.html", file = "Rplot.png",
-        cliprect = "viewport")
-
-
-m <- leaflet() %>% addTiles()
-mapshot(m, file = "Rplot.png")
-
-## 'mapview' objects (image below)
-m2 <- mapview(breweries91)
-mapshot(m2, file = "breweries.png")
