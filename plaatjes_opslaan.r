249e068c0896b5eb64d421524f1a223b54161ce7
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-13 22:14:55 +0200

    x

--- plaatjes_opslaan.r
+++ plaatjes_opslaan.r
@@ -29,7 +29,7 @@ nummers = setdiff(c( which(shape@data$goed == 1), which(shape@data$goed == 2) ),
 
 
 ###########maak cluster aan
-no_cores <- detectCores() - 4
+no_cores <- detectCores() - 1
 cl <- makeCluster(no_cores)
 
 clusterCall(cl, function() { 
