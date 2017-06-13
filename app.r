4277bb0674a2cc5a8cbddceb8356107f7e855486
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-13 10:20:54 +0200

    x

--- app.r
+++ app.r
@@ -4,8 +4,12 @@ source('packages.r')
 source('lees_shape.r')
 #reken de coordinaten om naar wsg en sla op als shape_wgs.rds
 source('omrekenen_shape.r')
-#teken op de kaart
+#teken op de kaart en label de plaatjes
 source('leaflet.r')
-
+#sla de plaatjes op als een laag voor de achtergrond en eenn laag voor de lijn
+source('plaatjes_opslaan.r')
+#Maak een neuraalnetwerk input van de plaatjes (toekomstig)
+#train neuraalnetwerk en sla het op (toekomstig)
+#laad neuraalnetwerk (toekomstig)
 
 
