52f59d8697f724e741c9660e8d97061730f0b9c6
daanvandermaas <daanvandermaas@hotmail.com>
2017-06-14 15:50:06 +0200

    x

--- raster_wegvakken.r
+++ raster_wegvakken.r
@@ -1,4 +1,6 @@
-
+if(!dir.exists('db/raster_plaatjes')){
+  dir.create('db/raster_plaatjes')
+}
 shape <- readRDS('db/shape_wgs.rds')
 
 maxs = raster_rechts
@@ -19,8 +21,22 @@ coords <- ldply(coords_list, data.frame)
 
 colnames(coords) = c('nummer', 'lat', 'lon')
 
-#vind alle nummers waarvoor coordinaten ertussen liggen
-for(N in 1:nrow(coords)){
+####maak cluster
+no_cores <- detectCores() - 1
+
+cl <- makeCluster(no_cores)
+
+clusterCall(cl, function() { 
+  library(rPython)
+  py = python.load("omrekenen.py")
+  
+})
+
+clusterExport(cl, c('shape', 'maxs', 'mins'))
+#############
+
+parSapply(cl, c(1:nrow(coords)), function(N){
+
 
 
 nummers = unique(coords$nummer[(coords$lat > mins$lat[N] )  &  (coords$lat < maxs$lat[N] )  & ( coords$lon > mins$lon[N]   )    & ( coords$lon < maxs$lon[N]   )])
@@ -33,11 +49,11 @@ if(length(nummers)>0){
 
 
 wegvakken = lapply(nummers, function(i){
-  print(i)
   return(shape@lines[[i]][[1]])
   
 })
 
+png(paste0('db/raster_plaatjes/', N, '.png'))
 plot(x = c(maxs$lon[N], mins$lon[N]), y = c(maxs$lat[N], mins$lat[N]) )
 
 
@@ -45,17 +61,12 @@ for(i in 1:length(wegvakken)){
   lines(x= wegvakken[[i]][,2],  y = wegvakken[[i]][,1] )
 }
 
+dev.off()
 
 
+}#eind ifstatement
 
-
-
-}
-
-}
-
-
-
+})
 
 
 
@@ -78,16 +89,19 @@ for(i in 1:length(wegvakken)){
 
 
 
-map = leaflet() 
-map = addTiles(map)
-
-for(i in 1:length(wegvakken)){
-  map =   addPolylines(map, lat= wegvakken[[i]][,1] , lng = wegvakken[[i]][,2] )
-}
 
 
-map = addCircleMarkers(map , lat= maxs$lat[N], lng = maxs$lon[N], color = 'red')
 
-map = addCircleMarkers(map , lat= mins$lat[N], lng = mins$lon[N], color = 'red')
-print(map)
-print(N)
+#map = leaflet() 
+#map = addTiles(map)
+
+#for(i in 1:length(wegvakken)){
+#  map =   addPolylines(map, lat= wegvakken[[i]][,1] , lng = wegvakken[[i]][,2] )
+#}
+
+
+#map = addCircleMarkers(map , lat= maxs$lat[N], lng = maxs$lon[N], color = 'red')
+
+#map = addCircleMarkers(map , lat= mins$lat[N], lng = mins$lon[N], color = 'red')
+#print(map)
+#print(N)
